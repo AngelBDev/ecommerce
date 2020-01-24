bb3280d15ce91022c8ce0e19f88029b78160434a
AngelBDev <46860812+AngelBDev@users.noreply.github.com>
2020-01-24 16:36:22 -0400

    broken page in smaller components and added style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import Homepage from './components/homepage/Homepage.component';
+import Homepage from './pages/homepage/Homepage.component';
 
 function App() {
 	return (
